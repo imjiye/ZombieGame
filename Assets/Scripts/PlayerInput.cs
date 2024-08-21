3975b0c849891a17c12c751cbc9ca88d005093b9
imjiye <imjyjy17@gmail.com>
2024-08-21 14:34:13 +0900

    24.08.21 수업내용

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,7 +2,8 @@
 
 // 플레이어 캐릭터를 조작하기 위한 사용자 입력을 감지
 // 감지된 입력값을 다른 컴포넌트들이 사용할 수 있도록 제공
-public class PlayerInput : MonoBehaviour {
+public class PlayerInput : MonoBehaviour 
+{
     //project setting에 있는 input manager에 있는 aixs와 동일한 이름들임.
     public string moveAxisName = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
     public string rotateAxisName = "Horizontal"; // 좌우 회전을 위한 입력축 이름
@@ -12,14 +13,17 @@ public class PlayerInput : MonoBehaviour {
     // 값 할당은 내부에서만 가능
     //프로퍼티 : 변숫값을 읽거나 쓰는 과정에서 유연한 처리를 삽입 할 수 있는 클래스 멤버.
     //          변수처럼 보이지만 변수가 아닌 특수한 형태의 메서드.
+    //          get, set 같은 접근자 함수를 포함하고 있음.
     public float move { get; private set; } // 감지된 움직임 입력값
     public float rotate { get; private set; } // 감지된 회전 입력값
     public bool fire { get; private set; } // 감지된 발사 입력값
     public bool reload { get; private set; } // 감지된 재장전 입력값
 
     // 매프레임 사용자 입력을 감지
-    private void Update() {
+    private void Update() 
+    {
         // 게임오버 상태에서는 사용자 입력을 감지하지 않는다
+        //GameManager가 한번이라도 만들어졌는지 확인함
         if (GameManager.instance != null && GameManager.instance.isGameover)
         {
             move = 0;
