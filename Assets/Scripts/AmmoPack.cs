8b4f50e08166c47ae29deaacd85984e1f9cf837a
imjiye <imjyjy17@gmail.com>
2024-09-04 11:18:24 +0900

    24.09.04 수업내용

--- Assets/Scripts/AmmoPack.cs
+++ Assets/Scripts/AmmoPack.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 
 // 총알을 충전하는 아이템
-public class AmmoPack : MonoBehaviour, IItem {
+public class AmmoPack : MonoBehaviour, IItem 
+{
     public int ammo = 30; // 충전할 총알 수
 
-    public void Use(GameObject target) {
+    public void Use(GameObject target) 
+    {
         // 전달 받은 게임 오브젝트로부터 PlayerShooter 컴포넌트를 가져오기 시도
         PlayerShooter playerShooter = target.GetComponent<PlayerShooter>();
 
@@ -13,6 +15,7 @@ public class AmmoPack : MonoBehaviour, IItem {
         {
             // 총의 남은 탄환 수를 ammo 만큼 더한다
             playerShooter.gun.ammoRemain += ammo;
+            //playershooter에서  update()로 계속 매프레임마다 UpdateUI를 하고 있기에 알아서 바뀜.
         }
 
         // 사용되었으므로, 자신을 파괴
